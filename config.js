5bfa4584e15a7b9dd9d73eaec57a19b996ddda35
DarkWinzo <93755320+DarkWinzo@users.noreply.github.com>
2022-09-05 16:47:47 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,146 @@
+/*
+   ██████╗ ██╗   ██╗███████╗███████╗███╗   ██╗      ███╗   ███╗██████╗ 
+  ██╔═══██╗██║   ██║██╔════╝██╔════╝████╗  ██║      ████╗ ████║██╔══██╗
+  ██║   ██║██║   ██║█████╗  █████╗  ██╔██╗ ██║█████╗██╔████╔██║██║  ██║
+  ██║▄▄ ██║██║   ██║██╔══╝  ██╔══╝  ██║╚██╗██║╚════╝██║╚██╔╝██║██║  ██║   
+  ╚██████╔╝╚██████╔╝███████╗███████╗██║ ╚████║      ██║ ╚═╝ ██║██████╔╝   
+   ╚══▀▀═╝  ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═══╝      ╚═╝     ╚═╝╚═════╝   
+  
+══════════════════════════════════════════════════════════════════════════
+                            ( OWNER  DETAILS )
+══════════════════════════════════════════════════════════════════════════
+✅ Queen Multi-device Whatsapp Bot
+✅ Coded By DarkWinzo 
+══════════════════════════════════════════════════════════════════════════
+                              ( CONTACT ME )
+══════════════════════════════════════════════════════════════════════════
+✅ Telegram: https://t.me/DarkWinzo
+✅ Whatsapp: https://wa.link/jw9ki6
+✅ Instagram:https://msng.link/o/?darkwinzo=ig
+✅ Youtube: https://youtube.com/c/DarkWinzo                                                                                                      
+══════════════════════════════════════════════════════════════════════════
+                             ( PROJECT RULES )
+══════════════════════════════════════════════════════════════════════════
+✅ If you want to recode, reupload
+   or copy the codes/script,
+   please Contact me;
+✅ If the recoded/reupload or copy script? "i will take action immediately"
+✅ © 2022 Queen Bot Inc.Technical Hacker Team
+✅ God Bless You, Family and Myself
+══════════════════════════════════════════════════════════════════════════
+*/
+
+const fs = require("fs")
+const chalk = require("chalk")
+
+global.available = true
+global.autoReadGc = true
+global.autoReadAll = false
+global.antitags = true
+
+
+global.Owner = ['94775200935']
+global.OwnerNumber = ['94775200935']
+global.ownertag = ['94775200935']
+global.BotName = "Queen-MD Bot"
+global.packname = "Queen Bot"
+global.author = "DarkWinzo"
+global.OwnerName = "DarkWinzo"
+global.BotSourceCode = "https://github.com/DarkWinzo/Queen-MD"
+global.SupportGroupLink = "https://chat.whatsapp.com/IWvaZWObbJK0Dr64WQdGqO"
+global.sessionName = "session"
+
+
+global.prefa = ['-'] 
+
+
+global.location = "Galle,Sri Lanka"
+global.reactmoji = "❤️"
+global.themeemoji = "💖"
+global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
+global.websitex = "https://github.com/DarkWinzo
+global.lolhuman = "KaysaS"
+
+
+global.BotLogo = fs.readFileSync("./Assets/pic1.jpg")
+global.Thumb = fs.readFileSync("./Assets/pic9.jpg")
+global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg")
+global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg")
+
+
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+global.mess = {
+    jobdone: 'වැඩේ ඉවරයි...',
+    useradmin: 'සමාවන්න,මෙම විධානය භාවිතා කළ හැක්කේ *Group Admins* පමණි. *මෝඩයා* !',
+    botadmin: 'සමාවන්න,මට මෙම කණ්ඩායමේ *පරිපාලක (Admin)* වරයෙකු නොවී මෙම විධානය ක්‍රියාත්මක කළ නොහැක.',
+    botowner: 'මෙම විධානය භාවිතා කළ හැක්කේ මගේ *හිමිකරුට* පමණි,මෝඩයා!',
+    grouponly: 'මෙම විධානය සාදා ඇත්තේ *Groups* සඳහා පමණි,මෝඩයා!',
+    privateonly: 'මෙම විධානය සාදා ඇත්තේ *Private Chats* සඳහා පමණි,මෝඩයා!',
+    botonly: 'මෙම විධානය භාවිතා කළ හැක්කේ *Bot ම* පමණි!',
+    waiting: 'මදක් රැදී සිටින්න...',
+    nolink: 'කරුණාකර මට *Link එක* ලබා දෙන්න,මෝඩයා!',
+    error: 'කිසියම් දෝෂයක් සිදු විය!',
+    banned: 'ඔබට විධාන භාවිතා කිරීම තහනම් කර ඇත!',
+    bangc: 'මෙම Group එකට විධාන භාවිතා කිරීම තහනම් කර ඇත!',
+    nonsfw: 'මෝඩයෙක් වෙන්න එපා! මෙය NSFW සබල කළ කණ්ඩායමක් නොවේ!'
+    
+}
+
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 2,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 95,
+   goldawal: 30,
+   emeraldawal: 8,
+   umpanawal: 10,
+   potionawal: 5
+}
+global.limitawal = {
+    rakyat: "Infinity",
+    free: 100
+}
+
+
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+global.APIKeys = {
+	'https://zenzapis.xyz': '5d1197db351b',
+}
+
+
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
